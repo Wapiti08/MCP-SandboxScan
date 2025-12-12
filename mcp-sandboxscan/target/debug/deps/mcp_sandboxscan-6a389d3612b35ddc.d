8d63870a644bd24f9487f4f173ba7ac67b01178d
Wapiti08 <tzrzhuoran@gmail.com>
2025-12-12 14:47:49 +0000

    update basic structure

--- mcp-sandboxscan/target/debug/deps/mcp_sandboxscan-6a389d3612b35ddc.d
+++
@@ -1,5 +0,0 @@
-/Users/zhuoran/Projects/MCP-SandboxScan/mcp-sandboxscan/target/debug/deps/mcp_sandboxscan-6a389d3612b35ddc.d: src/main.rs
-
-/Users/zhuoran/Projects/MCP-SandboxScan/mcp-sandboxscan/target/debug/deps/libmcp_sandboxscan-6a389d3612b35ddc.rmeta: src/main.rs
-
-src/main.rs:
